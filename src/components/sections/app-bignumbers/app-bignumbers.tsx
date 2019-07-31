26829d995a1858287b45aa294674848c73906c22
Luiz Guilherme Gomes dos Santos <luizsantos@reitoria.ufrj.br>
2019-07-31 11:12:08 -0300

    Término de mnv-bignumbers

--- src/components/sections/app-bignumbers/app-bignumbers.tsx
+++ src/components/sections/app-bignumbers/app-bignumbers.tsx
@@ -14,16 +14,16 @@ export class AppBignumbers {
 				</div>
 				<mnv-bignumber /> */}
 				<mnv-grid container>
-					<mnv-grid item xl='2' />
-					<mnv-grid item xl='8'>
+					<mnv-grid item xl='2' lg='2' />
+					<mnv-grid item xl='8' lg='8' md='12' sm='12'>
 						<mnv-title level='h1'>UFRJ em números</mnv-title>
 					</mnv-grid>
-					<mnv-grid item xl='2' />
-					<mnv-grid item xl='2' />
-					<mnv-grid item xl='8'>
+					<mnv-grid item xl='2' lg='2' />
+					<mnv-grid item xl='2' lg='2' />
+					<mnv-grid item xl='8' lg='8' md='12' sm='12'>
 						<mnv-bignumber />
 					</mnv-grid>
-					<mnv-grid item xl='2' />
+					<mnv-grid item xl='2' lg='2' />
 				</mnv-grid>
 			</div>
 		)
